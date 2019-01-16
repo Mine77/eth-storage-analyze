e13ad9eaf3265e0e7a3cbe41033d54374649cc9d
Haichao <haichaozhu77@gmail.com>
2019-01-16 22:27:17 +0800

    update: stream stateTrie and storageTrie works

--- index.js
+++ index.js
@@ -1,34 +1,78 @@
-var Trie = require('merkle-patricia-tree');
+var Trie = require('merkle-patricia-tree/secure');
 var levelup = require('levelup');
 var leveldown = require('leveldown');
+var rlp = require('rlp');
 
-// var Web3 = require('web3');
+const emptyStateRoot = '56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421';
 
-var stateRoot = "0xd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544"
-
+// Connect to the chaindata db
 var db = levelup(leveldown('/Users/haichaozhu/Library/Ethereum/geth/chaindata'));
-var trie = new Trie(db,stateRoot);
 
-// Using the IPC provider in node.js
-// var net = require('net');
+// streamTrie and print a list of keys and values
+function streamStateTrie(stateroot) {
+    var trie = new Trie(db, stateroot);
+    var count = 0;
+    var contractCount = 0;
+    trie.createReadStream()
+        .on('data', function (data) {
 
-// var web3 = new Web3('/Users/myuser/Library/Ethereum/geth.ipc', net); // mac os path
+            //accouts are rlp encoded
+            var decodedVal = rlp.decode(data.value);
+            stateRoot = decodedVal[2].toString('hex');
+            count = count + 1;
 
-// console.log(web3.eth.getBlock(blockHashOrBlockNumber));
+            if (stateRoot != emptyStateRoot) {
 
-trie.createReadStream()
-  .on('data', function (data) {
-    console.log(data)
-  })
-  .on('end', function() {
-    console.log('End.')
-  })
+                // for debugging
+                // console.log(stateRoot);
+                // console.log('\n');
 
+                streamStorageTrie(data.key.toString('hex'), '0x' + stateRoot)
+                contractCount = contractCount + 1;
+            }
 
-  
+        })
+        .on('end', function () {
+            console.log('Contract Count:' + contractCount);
+            console.log('All Account Count:' + count);
+        })
+}
 
+// stream storage trie and print its size
+function streamStorageTrie(key, storageroot) {
+    var trie = new Trie(db, storageroot);
+    var count = 0;
+    trie.createReadStream()
+        .on('data', function (data) {
 
-// or
-// var web3 = new Web3(new Web3.providers.IpcProvider('/Users/myuser/Library/Ethereum/geth.ipc', net)); // mac os path
-// on windows the path is: "\\\\.\\pipe\\geth.ipc"
-// on linux the path is: "/users/myuser/.ethereum/geth.ipc"
+            var decodedVal = rlp.decode(data.value);
+            count = count + 1;
+            // for debugging
+            // console.log('key:' + data.key.toString('hex'));
+            // console.log('value:' + decodedVal.toString('hex'));
+        })
+        .on('end', function () {
+            console.log('key:' + key);
+            console.log('Storage Counts:' + count);
+            // for debugging
+            // console.log('Stream Finished!');
+        })
+}
+
+function getPath(root, key) {
+    var trie = new Trie(db, root);
+    trie.findPath(key, function (err, node, keyRemainder, stack) {
+        if (err) return cb(err)
+        console.log(stack);
+    })
+}
+
+var stateRoot = "0x435f5b24c848fee3e68bf9abb63c7a43da720f0fa554dfa0fee5dd65c26879fb"
+var storageRoot = "0x66f4b2aa02ea0500c90c595b57d4a30aad982e1723db282eb0de8e641fdb0f9e"
+// var accountKey = "4f18656fc3fd36629f534929f19836d522e79a2c11eeefe37987118672b5747e"
+
+// streamStateTrie(stateRoot);
+
+// streamStorageTrie(storageRoot);
+
+// getPath(stateRoot, accountKey)
