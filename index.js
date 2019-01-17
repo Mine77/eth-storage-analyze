43b5c2c31ff8c99256b42d967b1b19691801d819
Haichao <haichaozhu77@gmail.com>
2019-01-17 14:06:06 +0800

    refactor: refactor index.js

--- index.js
+++ index.js
@@ -1,80 +1,61 @@
 const Trie = require('merkle-patricia-tree/secure');
-const levelup = require('levelup');
-const leveldown = require('leveldown');
-const rlp = require('rlp');
+const Levelup = require('levelup');
+const Leveldown = require('leveldown');
+const RLP = require('rlp');
+const Config = require("./config.json")
 
-const config = require("./config.json")
-
-const emptyStateRoot = '56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421';
+const emptyStorageRoot = '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421';
 
 // Connect to the chaindata db
-const db = levelup(leveldown(config.DB_ADDRESS));
+const db = Levelup(Leveldown(Config.DB_ADDRESS));
 
-// streamTrie and print a list of keys and values
-function streamStateTrie(stateroot) {
-    const trie = new Trie(db, stateroot);
-    var count = 0;
-    var contractCount = 0;
-    trie.createReadStream()
-        .on('data', function (data) {
-
-            //accouts are rlp encoded
-            var decodedVal = rlp.decode(data.value);
-            stateRoot = decodedVal[2].toString('hex');
-            count = count + 1;
-
-            if (stateRoot != emptyStateRoot) {
-
-                // for debugging
-                // console.log(stateRoot);
-                // console.log('\n');
-
-                streamStorageTrie(data.key.toString('hex'), '0x' + stateRoot)
-                contractCount = contractCount + 1;
-            }
-
-        })
-        .on('end', function () {
-            console.log('Contract Count:' + contractCount);
-            console.log('All Account Count:' + count);
-        })
-}
-
-// stream storage trie and print its size
-function streamStorageTrie(key, storageroot) {
-    const trie = new Trie(db, storageroot);
-    var count = 0;
-    trie.createReadStream()
-        .on('data', function (data) {
-
-            var decodedVal = rlp.decode(data.value);
-            count = count + 1;
-            // for debugging
-            // console.log('key:' + data.key.toString('hex'));
-            // console.log('value:' + decodedVal.toString('hex'));
-        })
-        .on('end', function () {
-            console.log('key:' + key);
-            console.log('Storage Counts:' + count);
-            // for debugging
-            // console.log('Stream Finished!');
-        })
-}
-
-function getPath(root, key) {
-    var trie = new Trie(db, root);
-    trie.findPath(key, function (err, node, keyRemainder, stack) {
-        if (err) return cb(err)
-        console.log(stack);
+// streamTrie and return all the key and values
+function streamTrie(db, root) {
+    return new Promise(function (resolve, reject) {
+        const trie = new Trie(db, root);
+        var allData = {};
+        trie.createReadStream()
+            .on('data', function (data) {
+                //values are rlp encoded
+                allData[data.key.toString('hex')] = RLP.decode(data.value);
+            })
+            .on('end', function () {
+                resolve(allData);
+                // console.log('Stream Finished');
+            })
+            .on('error', function (err) {
+                reject(new Error(err));
+            })
     })
 }
 
-var stateRoot = "0x435f5b24c848fee3e68bf9abb63c7a43da720f0fa554dfa0fee5dd65c26879fb"
-var storageRoot = "0x66f4b2aa02ea0500c90c595b57d4a30aad982e1723db282eb0de8e641fdb0f9e"
-// var accountKey = "4f18656fc3fd36629f534929f19836d522e79a2c11eeefe37987118672b5747e"
+function calculateStorageSize(db,storageRoot) {
+    return new Promise(function(resolve,reject) {
+        streamTrie(db,storageRoot).then(data => {
+            resolve(Object.keys(data).length);
+        }).catch(function onRejected(error){
+            console.error(error);
+        });
+    })
+}
 
-// streamStateTrie(stateRoot);
+const stateRoot = "0x435f5b24c848fee3e68bf9abb63c7a43da720f0fa554dfa0fee5dd65c26879fb"
+const testStorageRoot = "0x66f4b2aa02ea0500c90c595b57d4a30aad982e1723db282eb0de8e641fdb0f9e"
+const accountKey = "8f22c90535b94094379767ebdf7bff46efc7427425b2b3fb1684538b88274523"
 
-// streamStorageTrie(storageRoot);
+// calculateStorageSize(db,testStorageRoot);
 
-// getPath(stateRoot, accountKey)
+streamTrie(db, stateRoot)
+    .then(data => {
+        Object.values(data).forEach(value => {
+            // storageRoot needs 0x as prefix
+            var storageRoot = '0x' + value[2].toString('hex');
+            var storageSize = [];
+            if (storageRoot != emptyStorageRoot) {
+                calculateStorageSize(db,storageRoot).then(console.log);
+                // console.log(storageRoot);
+            }
+        })
+    });
+
+// streamStorageTrie(db,storageRoot);
